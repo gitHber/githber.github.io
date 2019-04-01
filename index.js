28d7820528f15371db69db0262dfc7e2d79968f9
likun <1617455243@qq.com>
2019-04-01 19:28:38 +0800

    complete

---
+++ index.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+  (function() {
+    var no = Math.floor(Math.random() * 9) + 1;
+    document.getElementById(
+      "container"
+    ).style.backgroundImage = `url(./images/wallpaper/bg-cover${no}.jpg)`;
+  })();
+  document.body.addEventListener("mouseup", function() {
+    Array.from(document.getElementsByClassName("dropdown-menu")).map(function(
+      node
+    ) {
+      node.parentNode.classList.remove("open");
+    });
+  });
+  for (let node of document.getElementsByClassName("drop-btn")) {
+    node.addEventListener("mouseup", function(e) {
+      e.stopPropagation();
+    });
+    node.addEventListener("click", function(e) {
+      e.target.parentNode.classList.add("open");
+    });
+  }
+};
