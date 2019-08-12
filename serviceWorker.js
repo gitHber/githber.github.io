baee1dc0244556b4deb444a4fa48b90950c53280
likun <1617455243@qq.com>
2019-08-12 12:48:09 +0800

    feature: add service worker

---
+++ serviceWorker.js
@@ -0,0 +1,73 @@
+var cacheStorageKey = "lk190812";
+var cacheList = [
+  "/",
+  "index.html",
+  "index.js",
+  "index.css",
+  "dot-line.js",
+  "favicon.ico",
+  "images/",
+  "/book/index.html",
+  "/book/index.js",
+  "/book/index.css"
+];
+// 安装
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches
+      .open(cacheStorageKey)
+      .then(cache => {
+        return cache.addAll(cacheList);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+// 处理动态缓存
+self.addEventListener("fetch", e => {
+  const { request } = e;
+  e.respondWith(
+    caches.match(request).then(response => {
+      if (response) {
+        return response;
+      }
+      // 对于 CDN 资源要更改 request 的 mode
+      if (
+        request.mode !== "navigate" &&
+        request.url.indexOf(request.referrer) === -1
+      ) {
+        request = new Request(request, { mode: "cors" });
+      }
+      return fetch(request).then(fetchRes => {
+        if (!fetchRes || fetchRes.status !== 200 || request.method !== "GET") {
+          return fetchRes;
+        }
+        let resClone = fetchRes.clone();
+        caches.open(cacheStorageKey).then(cache => {
+          cache.put(request, fetchRes);
+        });
+        return resClone;
+      });
+    })
+  );
+});
+// 激活
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches
+      .keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames
+            .filter(cacheName => {
+              return cacheName !== cacheStorageKey;
+            })
+            .map(cacheName => {
+              return caches.delete(cacheName);
+            })
+        );
+      })
+      .then(() => {
+        return self.clients.claim();
+      })
+  );
+});
